2654645ff350b0475572bd23cc3916dcd4b2a053
gab-braga <gabrielbraga0712@gmail.com>
2025-04-18 00:39:08 -0300

    fix: recording router updated

--- src/routes/recordingRouter.ts
+++ src/routes/recordingRouter.ts
@@ -1,42 +1,46 @@
-import { Router } from 'express';
-import checkAuthToken from '../middlewares/checkAuthToken';
-import { findById, findWithFilters, save, update } from '../firebase/db';
-import Sort from '../interfaces/Sort';
-import Filter from '../interfaces/Filter';
-import { transcribeAudio } from '../services/openaiService';
+import { Router } from "express";
+import checkAuthToken from "../middlewares/checkAuthToken";
+import { findById, findWithFilters, save, update } from "../firebase/db";
+import Sort from "../interfaces/Sort";
+import Filter from "../interfaces/Filter";
+import { transcribeAudio } from "../services/openaiService";
 
 const recordingRouter = Router();
 
-recordingRouter.post('/recordings', checkAuthToken, async (req, res) => {
+recordingRouter.post("/recordings", checkAuthToken, async (req, res) => {
   try {
     const user = req.user;
 
     if (!user) {
       return res.status(401).json({
-        message: 'Você precisa estar logado para acessar este recurso.',
+        message: "Você precisa estar logado para acessar este recurso.",
       });
     }
 
-    const { classId, recordingUrl, recordingStartTime, recordingStopTime } =
-      req.body;
+    const {
+      classId,
+      recordingPartsURLs,
+      recordingStartTime,
+      recordingStopTime,
+    } = req.body;
 
     if (
       !classId ||
-      !recordingUrl ||
+      !recordingPartsURLs ||
       !recordingStartTime ||
       !recordingStopTime
     ) {
       return res.status(400).json({
         message:
-          'Algumas informações estão faltando. Verifique e tente novamente.',
+          "Algumas informações estão faltando. Verifique e tente novamente.",
       });
     }
 
-    const result = await findById('classes', classId);
+    const result = await findById("classes", classId);
 
     if (!result) {
       return res.status(404).json({
-        message: 'Este recurso não foi encontrado.',
+        message: "Este recurso não foi encontrado.",
       });
     }
 
@@ -44,7 +48,7 @@ recordingRouter.post('/recordings', checkAuthToken, async (req, res) => {
 
     if (result.userId !== userId) {
       return res.status(401).json({
-        message: 'Você não tem autorização para acessar este recurso.',
+        message: "Você não tem autorização para acessar este recurso.",
       });
     }
 
@@ -52,26 +56,26 @@ recordingRouter.post('/recordings', checkAuthToken, async (req, res) => {
     const data = {
       userId,
       classId,
-      recordingUrl,
+      recordingPartsURLs,
       recordingStartTime,
       recordingStopTime,
       createdAt,
     };
 
-    const recording = await save('recordings', data);
+    const recording = await save("recordings", data);
 
     return res.status(200).json(recording);
   } catch (error) {
     console.error(error);
 
     return res.status(500).json({
-      message: 'Error interno no servidor. Tente novamente mais tarde.',
+      message: "Error interno no servidor. Tente novamente mais tarde.",
     });
   }
 });
 
 recordingRouter.get(
-  '/recordings/classes/:id',
+  "/recordings/classes/:id",
   checkAuthToken,
   async (req, res) => {
     try {
@@ -79,16 +83,16 @@ recordingRouter.get(
 
       if (!user) {
         return res.status(401).json({
-          message: 'Você precisa estar logado para acessar este recurso.',
+          message: "Você precisa estar logado para acessar este recurso.",
         });
       }
 
       const classId = req.params.id;
-      const result = await findById('classes', classId);
+      const result = await findById("classes", classId);
 
       if (!result) {
         return res.status(404).json({
-          message: 'Este recurso não foi encontrado.',
+          message: "Este recurso não foi encontrado.",
         });
       }
 
@@ -96,31 +100,31 @@ recordingRouter.get(
 
       if (result.userId !== userId) {
         return res.status(401).json({
-          message: 'Você não tem autorização para acessar este recurso.',
+          message: "Você não tem autorização para acessar este recurso.",
         });
       }
 
       const filters: Filter[] = [
-        { field: 'userId', value: userId },
-        { field: 'classId', value: classId },
+        { field: "userId", value: userId },
+        { field: "classId", value: classId },
       ];
-      const sort: Sort = { field: 'createdAt', direction: 'desc' };
+      const sort: Sort = { field: "createdAt", direction: "desc" };
 
-      const recordings = await findWithFilters('recordings', filters, sort);
+      const recordings = await findWithFilters("recordings", filters, sort);
 
       return res.status(200).json(recordings);
     } catch (error) {
       console.error(error);
 
       return res.status(500).json({
-        message: 'Error interno no servidor. Tente novamente mais tarde.',
+        message: "Error interno no servidor. Tente novamente mais tarde.",
       });
     }
-  },
+  }
 );
 
 recordingRouter.post(
-  '/recordings/:id/transcription',
+  "/recordings/:id/transcription",
   checkAuthToken,
   async (req, res) => {
     try {
@@ -128,16 +132,16 @@ recordingRouter.post(
 
       if (!user) {
         return res.status(401).json({
-          message: 'Você precisa estar logado para acessar este recurso.',
+          message: "Você precisa estar logado para acessar este recurso.",
         });
       }
 
       const recordingId = req.params.id;
-      const recording = await findById('recordings', recordingId);
+      const recording = await findById("recordings", recordingId);
 
       if (!recording) {
         return res.status(404).json({
-          message: 'Este recurso não foi encontrado.',
+          message: "Este recurso não foi encontrado.",
         });
       }
 
@@ -145,29 +149,36 @@ recordingRouter.post(
 
       if (recording.userId !== userId) {
         return res.status(401).json({
-          message: 'Você não tem autorização para acessar este recurso.',
+          message: "Você não tem autorização para acessar este recurso.",
         });
       }
 
-      if (!recording.recordingUrl) {
+      const { recordingPartsURLs } = recording;
+
+      if (!recordingPartsURLs || !Array.isArray(recordingPartsURLs)) {
         return res.status(400).json({
-          message: 'Esta gravação não foi salva corretamente.',
+          message: "Esta gravação não foi salva corretamente.",
         });
       }
 
-      const transcription = await transcribeAudio(recording.recordingUrl);
-      const data = { transcription: transcription.text };
-      const recordingUpdated = await update('recordings', recording.id, data);
+      const promises = recordingPartsURLs.map((url) =>
+        transcribeAudio(url).then((transcription) => transcription.text)
+      );
+
+      const transcriptions = await Promise.all(promises);
+      const transcription = transcriptions.join(" ");
+      const data = { transcription };
+      const recordingUpdated = await update("recordings", recording.id, data);
 
       return res.status(200).json(recordingUpdated);
     } catch (error) {
       console.error(error);
 
       return res.status(500).json({
-        message: 'Error interno no servidor. Tente novamente mais tarde.',
+        message: "Error interno no servidor. Tente novamente mais tarde.",
       });
     }
-  },
+  }
 );
 
 export default recordingRouter;
