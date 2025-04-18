037792b37c1a98ff1f94a6a7287762e2b5bfe8f2
gab-braga <gabrielbraga0712@gmail.com>
2025-04-18 13:24:33 -0300

    fix: vercel deploy configuration removed

--- src/dev.ts
+++
@@ -1,3 +0,0 @@
-import app from './app/app';
-
-app.listen(3000, () => {});
--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,3 @@
-import { IncomingMessage, ServerResponse } from 'http';
-import { createServer } from 'http';
 import app from './app/app';
 
-export default function handler(req: IncomingMessage, res: ServerResponse) {
-  const server = createServer(app);
-  server.emit('request', req, res);
-}
+app.listen(3000, () => {});
