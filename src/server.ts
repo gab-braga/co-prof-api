1ed7e7bd7135a52d53d403113be93f5746295f01
gab-braga <gabrielbraga0712@gmail.com>
2025-04-20 12:20:56 -0300

    refactor: server.ts refactored

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,8 @@
 import app from "./app/app";
 
 const port = (process.env.PORT || 8080) as number;
+const host = process.env.HOST || "0.0.0.0";
 
-app.listen(port, "0.0.0.0", () => {
+app.listen(port, host, () => {
   console.log("Server running on port", port);
 });
